aa55d16f7146781d89cee4f34a64a472d1e9250a
wan.lin <wan.lin@gigacloudtech.com>
2024-09-23 10:33:57 +0800

    移除元素

---
+++ array/removeElement.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 2, 2, 3}
+	fmt.Println(removeElement(nums, 3))
+
+}
+
+func removeElement(nums []int, val int) int {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
+}
